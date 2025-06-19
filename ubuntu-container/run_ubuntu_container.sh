1162caa261a0c46f3723361cbe72789e7bafffd4
AvidDollars <dima.kol@seznam.cz>
2025-06-19 14:29:00 +0200

    delete option for running container interactively

--- ubuntu-container/run_ubuntu_container.sh
+++ ubuntu-container/run_ubuntu_container.sh
@@ -18,22 +18,11 @@ function _fn_exit_on_already_running_container() {
     fi
 }
 
-# interactive   -> user will have access to bash shell in running container
-# detached      -> container will be running in detached mode
-function _fn_ask_for_container_mode() {
-    read -p "Choose container mode: interactive/detached (i/d)? " choice
-    case "$choice" in 
-        i|I ) MODE="-it";;
-        d|D ) MODE="-itd";;
-        * ) >&2 echo "invalid option" && exit 69;;
-    esac
-}
-
 # Starts container with running SSH server.
 # IP address of running container is put into "inventory" file
 function _fn_run_container() {
     docker build -t $NAME . && \
-    docker run --rm $MODE --name $NAME $NAME /bin/bash -c '/etc/init.d/ssh start && /bin/bash' && \
+    docker run --rm -itd --name $NAME $NAME /bin/bash -c '/etc/init.d/ssh start && /bin/bash' && \
     IP=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" $NAME) && \
     ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R $IP && \
     ssh-copy-id -i ~/.ssh/ansible.pub root@$IP # interactive input, provide: "yes" && "root" (password)
@@ -43,6 +32,5 @@ function _fn_run_container() {
 
 ############# PROCEDURE #############
 _fn_exit_on_already_running_container
-_fn_ask_for_container_mode
 _fn_run_container
 ############# PROCEDURE #############
