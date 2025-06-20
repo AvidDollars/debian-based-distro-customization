ef47e27a3ff45d794686e934ed219d4b0058a02b
AvidDollars <dima.kol@seznam.cz>
2025-06-20 11:46:30 +0200

    add option to spin multiple containers

--- ubuntu-container/run_ubuntu_container.sh
+++ ubuntu-container/run_ubuntu_container.sh
@@ -1,14 +1,23 @@
-#!/usr/bin/bash
-set -e
+#!/usr/bin/env bash
 
 ###############################################################
 # author: Dimitrij Kolničenko
 #
 # functionality:
-#   - spins Ubuntu container with running SSH server
-#   - to be used as Ansible target for tesing Ansible playbooks
+#   - spins Ubuntu container(s) with running SSH server
+#   - to be used as Ansible target for tesing Ansible commands/playbooks
+#
+# usage:
+#   ./run_ubuntu_container
+#       ->  spins one container
+#
+#   ./run_ubuntu_container <count>
+#       ->  spins <count> number of containers
 ###############################################################
 
+set -e
+MAX_CONTAINERS=10
+CONTAINER_IDX=1
 NAME=ubu-ssh
 
 function _fn_exit_on_already_running_container() {
@@ -21,16 +30,36 @@ function _fn_exit_on_already_running_container() {
 # Starts container with running SSH server.
 # IP address of running container is put into "inventory" file
 function _fn_run_container() {
-    docker build -t $NAME . && \
-    docker run --rm -itd --name $NAME $NAME /bin/bash -c '/etc/init.d/ssh start && /bin/bash' && \
-    IP=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" $NAME) && \
+    local name=$NAME-$CONTAINER_IDX
+
+    docker build -t $name . && \
+    docker run --rm -itd --name $name $name /bin/bash -c '/etc/init.d/ssh start && /bin/bash' && \
+    IP=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" $name) && \
     ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R $IP && \
-    ssh-copy-id -i ~/.ssh/ansible.pub root@$IP # interactive input, provide: "yes" && "root" (password)
-    echo $IP > inventory
+    ssh-copy-id -i ~/.ssh/ansible.pub root@$IP # TODO: use "sshpass" so that script in non-interactive
+    echo $IP >> inventory
 }
 
 
-############# PROCEDURE #############
-_fn_exit_on_already_running_container
-_fn_run_container
-############# PROCEDURE #############
+################## MAIN PROCEDURE ##################
+if [[ $# -gt 0 ]]; then
+    iterations=$(($1)) # if error on int(input), result will be "0"
+
+    if (($iterations == 0)); then
+        >&2 echo "invalid argument '$1' provided" && exit 23
+    fi
+
+    if (($iterations > $MAX_CONTAINERS)); then
+        >&2 echo "OMFG! Too much containers... max is $MAX_CONTAINERS." && exit 13
+    fi
+fi
+
+echo -n > inventory # clears previous inventory file
+
+CONTAINERS_COUNT="$1"
+
+for idx in $(seq 1 $iterations); do
+    CONTAINER_IDX=$idx
+    _fn_exit_on_already_running_container
+    _fn_run_container
+done
